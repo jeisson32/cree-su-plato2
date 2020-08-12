aa2d3694a76c9f81adcff1cd289a0d54efa91b10
jeisson32 <65503825+jeisson32@users.noreply.github.com>
2020-08-11 21:35:52 -0500

    Add files via upload

---
+++ formulario/enviar.php
@@ -0,0 +1,9 @@
+<?php
+$destino="empresadearepasjmv@gmail.com";
+$nombre= $_POST["nombre"];
+$correo= $_POST["correo"];
+$mensaje= $_POST["mensaje"];
+$contenido = "Nombre:" . $nombre: ."\nCorreo:" . $correo .  "\nMensaje:" . $mensaje
+mail($destino,"Contacto", $contenido);
+header("location:gracias.html")
+?>
